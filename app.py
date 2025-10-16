8a15a3c1db759152a339fab61d13b2e1e0adc668
Anshul <24f1002114@ds.study.iitm.ac.in>
2025-10-17 00:03:52 +0530

    deleted app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-# app.py at root
-# Imports FastAPI instance from your folder
-from app.main import app
-
-# Optional: add startup events or logging if needed
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=7860)
